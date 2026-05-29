72d7c8897af632aaa1bb24f9fb11c747578f98c9
Richard Pidial <apidial@gmail.com>
2026-05-29 02:53:03 +0000

    Fix: tRPC context and dependency issues

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,3 +1,5 @@
 import { createTRPCReact } from "@trpc/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import type { AppRouter } from "../../server/routers";
 export const trpc = createTRPCReact<AppRouter>();
+export const queryClient = new QueryClient();
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { trpc, queryClient } from './lib/trpc'
+import { QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <trpc.Provider client={trpc.client} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </trpc.Provider>
   </React.StrictMode>,
 )
 
